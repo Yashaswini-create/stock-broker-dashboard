4455c61bbfc2997a414882e751a2514c61a30855
Yashaswini-create <yashaswinip204@gmail.com>
2026-01-08 16:10:55 +0530

    Prepare app for deployment

--- server.js
+++ server.js
@@ -45,6 +45,8 @@ io.on("connection", socket => {
 });
 
 // Start server
-server.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
